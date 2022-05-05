ca1c07e2622a131216712d4ca2718a74ee9a56b2
Alexander Eneroth <alexandereneroth@users.noreply.github.com>
2022-05-05 13:39:30 +0200

    Dont copy over private fields when copying english translation

--- src/structure/components/TranslationLink/TranslationLink.tsx
+++ src/structure/components/TranslationLink/TranslationLink.tsx
@@ -76,6 +76,7 @@ export const TranslationLink: React.FunctionComponent<IProps> = ({
       const { dontTranslateFields, ...baseDocumentFull } = baseDocument ? baseDocument : { dontTranslateFields: [] };
       const baseDocumentWithoutDontTranslateFields = Object.keys(baseDocumentFull)
           .reduce((acc, key) => {
+            if(key.startsWith('_')) return acc
             if(dontTranslateFields && dontTranslateFields.indexOf(key) !== -1)
               return acc
             acc[key] = baseDocumentFull[key]
